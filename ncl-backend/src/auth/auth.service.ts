955e676af8cce9bb3602f9313134b48ae8dd9732
Kirill Palkin <kirilpalkin@yandex.ru>
2024-10-15 23:10:35 +0500

    start auth service

--- ncl-backend/src/auth/auth.service.ts
+++ ncl-backend/src/auth/auth.service.ts
@@ -1,28 +1,16 @@
+import { UserService } from "@/user/user.service"
 import { Injectable } from "@nestjs/common"
-import { CreateAuthDto } from "./dto/create-auth.dto"
-import { UpdateAuthDto } from "./dto/update-auth.dto"
+import { JwtService } from "@nestjs/jwt"
+import { AuthDto } from "./dto/auth.dto"
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    console.log("createAuthDto: ", createAuthDto)
-    return "This action adds a new auth"
-  }
+  constructor(
+    private jwt: JwtService,
+    private userService: UserService
+  ) {}
 
-  findAll() {
-    return `This action returns all auth`
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    console.log("updateAuthDto: ", updateAuthDto)
-    return `This action updates a #${id} auth`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`
+  async login(dto: AuthDto) {
+    return dto
   }
 }
